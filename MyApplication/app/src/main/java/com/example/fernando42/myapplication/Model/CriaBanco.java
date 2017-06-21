85a0d548bc6915f8c44075a65079975cf86dcbb1
FernandoGrangeiro <fernandojgrangeiro@gmail.com>
2017-06-21 16:48:24 -0300

    Primeiro Commit

---
+++ MyApplication/app/src/main/java/com/example/fernando42/myapplication/Model/CriaBanco.java
@@ -0,0 +1,23 @@
+package com.example.fernando42.myapplication.Model;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Fernando42 on 21/06/2017.
+ */
+
+public class CriaBanco extends SQLiteOpenHelper {
+
+    private static final String NOME_Ba
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+
+}
